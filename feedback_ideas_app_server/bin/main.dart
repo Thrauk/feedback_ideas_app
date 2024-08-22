c043b12f4c4c1be359aef133f3083a5876a72526
Theodor Stoica <stoicath@gmail.com>
2024-08-22 15:17:33 +0300

    initial commit

---
+++ feedback_ideas_app_server/bin/main.dart
@@ -0,0 +1,26 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:sqlite3/open.dart';
+
+import 'package:feedback_ideas_app_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  open.overrideFor(OperatingSystem.linux, _openOnLinux);
+  open.overrideFor(OperatingSystem.windows, _openOnWindows);
+  run(args);
+}
+
+DynamicLibrary _openOnLinux() {
+  final scriptDir = File(Platform.script.toFilePath()).parent;
+  final libraryNextToScript = File(join(scriptDir.path, 'sqlite3', 'linux', 'sqlite3.so'));
+  return DynamicLibrary.open(libraryNextToScript.path);
+}
+
+DynamicLibrary _openOnWindows() {
+  final scriptDir = File(Platform.script.toFilePath()).parent;
+  final libraryNextToScript = File(join(scriptDir.path, 'sqlite3', 'windows', 'sqlite3.dll'));
+  return DynamicLibrary.open(libraryNextToScript.path);
+}
